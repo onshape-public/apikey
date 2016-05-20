6b818fa2c537c4fee8a9403531b1ae58cb67872a
Mauro Braunstein <mbraunstein@onshape.com>
2016-05-20 09:23:25 -0400

    Minor fixes to Node readme, better apikeyexample.js

--- Node/config/apikeyexample.js
+++ Node/config/apikeyexample.js
@@ -1,8 +1,15 @@
 // This is an example of a API key file.
 // Copy this to apikey.js in the same directory and replace the placeholders with appropriate values.
-
-module.exports = {
+var partner = {
   'baseUrl': 'https://partner.dev.onshape.com',
   'accessKey': ACCESS_KEY,
   'secretKey': SECRET_KEY
 };
+
+var prod = {
+	'baseUrl': 'https://cad.onshape.com',
+	'accessKey': ACCESS_KEY,
+	'secretKey': SECRET_KEY
+}
+
+module.exports = partner;
