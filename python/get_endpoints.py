999960fd8c217495231894f9a24cea1a3a927a86
Len Wanger <len_wanger@hotmail.com>
2018-10-30 14:46:58 -0500

    Utility to get documentation for onshape api. Added cooked_input table display and details for api endpoints.

--- python/get_endpoints.py
+++ python/get_endpoints.py
@@ -36,14 +36,88 @@ Returns a list of endpoints, each a dictionary with:
     this code uses  onshapepy to do authentication:
         https://github.com/lwanger/onshapepy
 
+TODO:
+    - export HTML of endpoints? Export as PDF from ReportLab
+
 Len Wanger
 Copyright Impossible Objects, 2018
 """
 
-# import pprint
+import pprint
 
 from onshapepy.ext_client import ClientExtended
 from onshapepy.utils import convert_response
+
+import cooked_input as ci
+
+
+
+def show_individual_endpoint_action(row, action_dict):
+    """
+    row.item_data['item'] is list of: type, url, title, name, description, group, version, permission (list), success (dict),
+        groupTitle, header (dict), parameter (dict)
+    """
+    ri = row.item_data["item"]
+    # print('-'*60)
+    print(f'\nname: {ri["name"]}\ttitle: {ri["title"]}')
+    print(f'description: {ri["description"]}')
+    print(f'url: {ri["url"]}')
+    print(f'type: {ri["type"]}\tversion: {ri["version"]}')
+    permissions = ','.join([ str(i) for i in ri["permission"] ])
+    print(f'permissions: {permissions}')
+
+    if 'success' in ri:
+        print()
+        response = ri['success']['fields']['Response']
+        fields = 'field group type optional description'.split()
+        field_names = [f.capitalize() for f in fields]
+        tbl = ci.create_table(items=response, fields=fields, field_names=field_names, gen_tags=False, add_exit=False, title='Response:')
+        tbl.show_table()
+
+    if 'success' in ri:
+        header = ri['header']['fields']['Header']
+        fields = 'field group type optional defaultValue description'.split()
+        field_names = [f.capitalize() for f in fields]
+        tbl = ci.create_table(items=header, fields=fields, field_names=field_names, gen_tags=False, add_exit=False,
+                              title='\nHeader:')
+        tbl.show_table()
+
+    if 'parameter' in ri:
+        if 'PathParam' in ri['parameter']['fields']:
+            path_param = ri['parameter']['fields']['PathParam']
+            fields = 'field group type optional description'.split()
+            field_names = [f.capitalize() for f in fields]
+            tbl = ci.create_table(items=path_param, fields=fields, field_names=field_names, gen_tags=False, add_exit=False,
+                                  title='PathParam:')
+            tbl.show_table()
+
+        if 'QueryParam' in ri['parameter']['fields']:
+            query_param = ri['parameter']['fields']['QueryParam']
+            fields = 'field group type optional defaultValue description'.split()
+            field_names = [f.capitalize() for f in fields]
+            tbl = ci.create_table(items=query_param, fields=fields, field_names=field_names, gen_tags=False, add_exit=False,
+                                  title='QueryParam:')
+            tbl.show_table()
+
+    print('\n\n\n')
+
+
+def show_group_endpoints_action(row, action_dict):
+    """
+    row.item_data['item'] has: 'group', 'groupTitle', 'endpoints'
+    row.item_data['item']['endpoints'] is list of: type, url, title, name, description, group, version, permission (list), success (dict),
+        groupTitle, header (dict), parameter (dict)
+    """
+    print(f'Received row for endpoint: {row.item_data["item"]["groupTitle"]}')
+
+    style = ci.TableStyle(rows_per_page=99)
+    items = row.item_data['item']['endpoints']
+    fields = 'title name type url'.split()
+    field_names = [f.capitalize() for f in fields]
+    tbl = ci.create_table(items=items, fields=fields, field_names=field_names, style=style, gen_tags=True,
+                          default_action=show_individual_endpoint_action,
+                          add_item_to_item_data=True, add_exit=ci.TABLE_ADD_RETURN, prompt='Chooose an endpoint')
+    tbl.run()
 
 
 if __name__ == '__main__':
@@ -54,16 +128,13 @@ if __name__ == '__main__':
     response = c.get_endpoints()
     endpoints = convert_response(response)
 
-    print('printing some samples:\n')
-    endpoint_18 = endpoints[18]['endpoints'][6]
-    print(f'endpoint 18 is: {endpoint_18["title"]} ({endpoint_18["name"]} - {endpoint_18["description"]})\n')
-    print(endpoint_18)
-    # pprint.pprint(endpoint_18)
-    print('\nget_parts parameter:\n')
-    print(endpoint_18['parameter'])
-    # pprint.pprint(endpoint_18['parameter'])
-    print('\nget_parts success:\n')
-    print(endpoint_18['success'])
-    # pprint.pprint(endpoint_18['success'])
-    print('\ndone')
+    # list endpoints as a table
+    style = ci.TableStyle(rows_per_page=99)
+    items = endpoints
+    fields = 'group groupTitle'.split()
+    field_names = [f.capitalize() for f in fields]
+    tbl = ci.create_table(items=items, fields=fields, field_names=field_names, style=style, gen_tags=True,
+                          default_action=show_group_endpoints_action, add_item_to_item_data=True, add_exit=True,
+                          prompt='Chooose an endpoint')
+    tbl.run()
 
